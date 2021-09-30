d5bfbcf429282eb686ef3d33af84c3518dac08be
lynnseah <seahlynn@u.nus.edu>
2021-09-30 14:31:36 +0800

    Changed product listing view to cards

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,18 +1,46 @@
 import { Component, OnInit } from '@angular/core';
-import { Tutorial } from 'src/app/models/tutorial.model';
+import { Product } from 'src/app/models/product.model';
 import { ProductService } from 'src/app/services/product.service';
+import { MatCardModule } from '@angular/material/card';
+
+
+const PRODUCT_DATA: Product[] = [
+    {
+            "id": 1,
+            "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+            "price": 109.95,
+            "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+            "category_id": 3,
+            "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+            "qty": 50
+        },
+        {
+            "id": 2,
+            "title": "Mens Casual Premium Slim Fit T-Shirts ",
+            "price": 22.3,
+            "description": "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
+            "category_id": 3,
+            "image": "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
+            "qty": 50
+        },
+];
+
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
+
 export class ProductListComponent implements OnInit {
 
   products?: Product[];
   //currentTutorial: Tutorial = {};
   //currentIndex = -1;
   //title = '';
+   productList = PRODUCT_DATA;
+   displayedColumns: string[] = ['title', 'description', 'price'];
+   dataSource = PRODUCT_DATA;
 
   constructor(private productService: ProductService) { }
 
